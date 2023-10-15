29e74bfe2482b4ab01c4f715de0b63fdd673a27e
kahadouskaya.nastassia <kahadouskaya.nastassia@gmail.com>
2023-10-15 19:17:35 +0300

    Calc fix

--- src/Calc.java
+++ src/Calc.java
@@ -1,9 +1,11 @@
 public class Calc {
     public String sum (String a, String b){
 
-        int number1 = Integer.parseInt(a);
-        int number2 = Integer.parseInt(b);
-        return Integer.toString(number1 + number2);
+//        int number1 = Integer.parseInt(a);
+//        int number2 = Integer.parseInt(b);
+//        return Integer.toString(number1 + number2);
+
+        return Integer.toString(Integer.parseInt(a) + Integer.parseInt(b));
     }
 }
 
