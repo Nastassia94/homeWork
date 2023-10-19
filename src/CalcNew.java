37198437c38d88e372e97ac63fe121eed3271ebe
Nastassia94 <58165065+Nastassia94@users.noreply.github.com>
2023-10-19 17:40:00 +0300

    Create CalcNew.java
    new Calc

---
+++ src/CalcNew.java
@@ -0,0 +1,14 @@
+public class Calc {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+    public int sub(int a, int b){
+        return a - b;
+    }
+    public int div(int a, int b){
+        return a / b;
+    }
+    public int mult(int a, int b){
+        return a * b;
+    }
+}
